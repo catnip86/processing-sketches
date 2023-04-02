fa7335bacefc3c7caf1780e462d96b764888e040
catnip <davide.fiore@gmail.com>
2023-04-02 22:51:05 +0200

    change button texts

--- sketches/ipnotic_torus/GifExporter.js
+++ sketches/ipnotic_torus/GifExporter.js
@@ -5,6 +5,7 @@ class GifExporter {
       this.frameCounter = 0;
       this.downloadButton = document.getElementById("download-gif");
       this.downloadButton.disabled = true;
+      this.downloadButton.textContent = "Collecting frames..."
       this.gifExport = new GIF({
         workers: 2,
         quality: 10,
@@ -30,10 +31,13 @@ class GifExporter {
     }
   
     captureFrame() {
+      camera(0, 0, 1000, 0, 0, 0, 0, 1, 0);
+
       if (this.frameCounter < this.numFrames) {
         this.gifExport.addFrame(canvas, { delay: 1000 / frameRate(), copy: true });
         this.frameCounter++;
       } else if (this.frameCounter === this.numFrames) {
+        this.downloadButton.textContent = "Rendering GIF...";
         this.gifExport.render();
         this.frameCounter++;
       }
