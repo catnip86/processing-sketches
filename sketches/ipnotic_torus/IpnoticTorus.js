35240b0227e696e001a52c4bec51a378ffa62ad4
catnip <davide.fiore@gmail.com>
2023-04-04 01:11:16 +0200

    change colors to array to fit when they have the same index

--- sketches/ipnotic_torus/IpnoticTorus.js
+++ sketches/ipnotic_torus/IpnoticTorus.js
@@ -1,11 +1,14 @@
 let movers = [];
 let glitchyBackground;
-let gifExporter;
 let loopDurationSeconds = 23;
-let fps = 60;
-let setupFinished = false;
+let fps = 30;
+let frameCounterSpan;
+let timeCounterSpan;
 
 function setup() {
+  frameCounterSpan = document.getElementById("frame-counter");
+  timeCounterSpan = document.getElementById("time-counter");
+
   let canvas = createCanvas(600, 600, WEBGL);
   canvas.willReadFrequently = true;
 
@@ -13,21 +16,23 @@ function setup() {
   colorMode(HSB, 360, 100, 100);
   frameRate(fps);
 
-  let synthPopColors = [
+  let colors1 = [
     color(300, 100, 100), // Pink
     color(200, 100, 100), // Cyan
     color(60, 100, 100),  // Yellow
     color(0, 100, 100)    // Red
   ];
   
-  let vaporWaveColors = [
+  // Color scheme 2
+  let colors2 = [
     color(327, 100, 100), // Hot Pink
-    color(45, 100, 100),  // Orange
     color(170, 100, 100), // Light Blue
-    color(110, 100, 100)  // Green
+    color(110, 100, 100), // Green
+    color(45, 100, 100)   // Orange
   ];
   
-  let psychedelicColors = [
+  // Color scheme 3
+  let colors3 = [
     color(280, 100, 100), // Purple
     color(10, 100, 100),  // Dark Red
     color(100, 100, 100), // Lime Green
@@ -39,42 +44,46 @@ function setup() {
   let toriNumber = 6;
 
   glitchyBackground = new GlitchyBackground();
-  gifExporter = new GifExporter(window, 'loop.gif', loopDurationSeconds, fps);
 
   for (let i = 0; i < 3; i++) {
-    let mover = new TorusMover(i / 3, torusSize, toriNumber, boxSize, synthPopColors);
+    let mover = new TorusMover(torusSize, toriNumber, boxSize, colors1);
     movers.push(mover)
   }
   
   for (let i = 0; i < 3; i++) {
-    let mover = new TorusMover(i / 3, torusSize * 1.5, toriNumber, boxSize * 1.5, vaporWaveColors);
+    let mover = new TorusMover(torusSize * 1.5, toriNumber, boxSize * 1.5, colors2);
     movers.push(mover)
   }
   
   for (let i = 0; i < 3; i++) {
-    let mover = new TorusMover(i / 3, torusSize * 2, toriNumber, boxSize * 2, psychedelicColors);
+    let mover = new TorusMover(torusSize * 2, toriNumber, boxSize * 2, colors3);
     movers.push(mover)
   }
 
-  setupFinished = true;
+  createLoop({
+    duration: loopDurationSeconds,
+    gif: {
+      fileName: "loop.gif",
+      download: true,
+      open: true,
+      options: {
+        workers: 10,
+        dither: "Atkinson"
+      }
+    },
+  });
 }
 
 function draw() {
-  if (!setupFinished) return;
-
   camera(0, 0, 1000, 0, 0, 0, 0, 1, 0);
 
   glitchyBackground.display();
 
   for (const mover of movers) {
-    mover.update(gifExporter.getTheta());
+    mover.update(animLoop.theta);
     mover.display();
   }
 
-  if (gifExporter.animLoop.completed()) {
-    gifExporter.animLoop.reset();
-  }
-
-  document.getElementById("frame-counter").innerText = `Frame: ${frameCount}`;
-  document.getElementById("time-counter").innerText = `Time: ${(millis() / 1000).toFixed(1)}s`;
+  frameCounterSpan.innerText = `Frame: ${frameCount}`;
+  timeCounterSpan.innerText = `Time: ${(millis() / 1000).toFixed(1)}s`;
 }
