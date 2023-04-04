ca5b599f14ebec2ec1cc59a6824c329556f4a7c9
catnip <davide.fiore@gmail.com>
2023-04-04 21:44:20 +0200

    add 3d rotating skull

--- sketches/ipnotic_torus/CyberPulseMorphosis.js
+++ sketches/ipnotic_torus/CyberPulseMorphosis.js
@@ -1,14 +1,25 @@
 let movers = [];
+
 let glitchyBackground;
+let rotatingSkull;
+
 let loopDurationSeconds = 23;
 let fps = 30;
+
 let frameCounterSpan;
 let timeCounterSpan;
+let thetaCounterSpan;
 
-function setup() {
+function preload() {
+  rotatingSkull = new RotatingSkull();
+  glitchyBackground = new GlitchyBackground();
+
   frameCounterSpan = document.getElementById("frame-counter");
   timeCounterSpan = document.getElementById("time-counter");
+  thetaCounterSpan = document.getElementById("theta-counter");
+}
 
+function setup() {
   let canvas = createCanvas(600, 600, WEBGL);
   canvas.willReadFrequently = true;
 
@@ -16,48 +27,56 @@ function setup() {
   colorMode(HSB, 360, 100, 100);
   frameRate(fps);
 
+  let boxSize = 200;
+  let torusSize = 40;
+  let toriNumber = 6;
+
   let colors1 = [
     color(300, 100, 100), // Pink
     color(200, 100, 100), // Cyan
-    color(60, 100, 100),  // Yellow
-    color(0, 100, 100)    // Red
+    color(60, 100, 100), // Yellow
+    color(0, 100, 100), // Red
   ];
-  
+
   // Color scheme 2
   let colors2 = [
     color(327, 100, 100), // Hot Pink
     color(170, 100, 100), // Light Blue
     color(110, 100, 100), // Green
-    color(45, 100, 100)   // Orange
+    color(45, 100, 100), // Orange
   ];
-  
+
   // Color scheme 3
   let colors3 = [
     color(280, 100, 100), // Purple
-    color(10, 100, 100),  // Dark Red
+    color(10, 100, 100), // Dark Red
     color(100, 100, 100), // Lime Green
-    color(220, 100, 100)  // Deep Sky Blue
+    color(220, 100, 100), // Deep Sky Blue
   ];
-  
-  let boxSize = 200;
-  let torusSize = 40;
-  let toriNumber = 6;
-
-  glitchyBackground = new GlitchyBackground();
 
   for (let i = 0; i < 3; i++) {
-    let mover = new TorusMover(torusSize, toriNumber, boxSize, colors1);
-    movers.push(mover)
+    let mover = new SpiralMover(torusSize, toriNumber, boxSize, colors1);
+    movers.push(mover);
   }
-  
+
   for (let i = 0; i < 3; i++) {
-    let mover = new TorusMover(torusSize * 1.5, toriNumber, boxSize * 1.5, colors2);
-    movers.push(mover)
+    let mover = new SpiralMover(
+      torusSize * 1.5,
+      toriNumber,
+      boxSize * 1.5,
+      colors2
+    );
+    movers.push(mover);
   }
-  
+
   for (let i = 0; i < 3; i++) {
-    let mover = new TorusMover(torusSize * 2, toriNumber, boxSize * 2, colors3);
-    movers.push(mover)
+    let mover = new SpiralMover(
+      torusSize * 2,
+      toriNumber,
+      boxSize * 2,
+      colors3
+    );
+    movers.push(mover);
   }
 
   createLoop({
@@ -68,23 +87,31 @@ function setup() {
       open: true,
       options: {
         workers: 10,
-        dither: "Atkinson"
-      }
+        dither: "Atkinson",
+      },
     },
   });
 }
 
 function draw() {
   camera(0, 0, 1000, 0, 0, 0, 0, 1, 0);
+  ambientLight(100, 100, 100);
+  directionalLight(255, 255, 255, 0, -1, -1);
 
-  glitchyBackground.update(animLoop.theta);
+  let theta = animLoop.theta;
+
+  glitchyBackground.update(theta);
+  rotatingSkull.update(theta);
+
   glitchyBackground.display();
+  rotatingSkull.display();
 
   for (const mover of movers) {
-    mover.update(animLoop.theta);
+    mover.update(theta);
     mover.display();
   }
 
   frameCounterSpan.innerText = `Frame: ${frameCount}`;
   timeCounterSpan.innerText = `Time: ${(millis() / 1000).toFixed(1)}s`;
+  thetaCounterSpan.innerText = `Theta: ${theta}`;
 }
--- sketches/ipnotic_torus/SpiralMover.js
+++ sketches/ipnotic_torus/SpiralMover.js
@@ -1,4 +1,4 @@
-class TorusMover {
+class SpiralMover {
   constructor(torusSize, toriNumber, boxSize, colors) {
     this.torusSize = torusSize;
     this.toriNumber = toriNumber;
@@ -9,23 +9,26 @@ class TorusMover {
   }
 
   update(theta) {
-    this.theta = theta;
+    this.theta = theta
     this.angle = this.theta;
     this.centerX = sin(this.theta) * 200;
     this.centerY = cos(this.theta) * 200;
-    this.centerZ = sin(this.theta * 2) * 100; 
+    this.centerZ = sin(this.theta * 2) * 100;
   }
 
   display() {
     push();
     let colorFrequency = 23;
-    let colorIndex = floor(map(this.angle, 0, TWO_PI, 0, this.colors.length * colorFrequency)) % this.colors.length;
+    let colorIndex =
+      floor(
+        map(this.angle, 0, TWO_PI, 0, this.colors.length * colorFrequency)
+      ) % this.colors.length;
     fill(this.colors[colorIndex]);
     noStroke();
 
-    let offsetX = this.centerX + sin(this.angle) * this.boxSize / 2;
-    let offsetY = this.centerY + cos(this.angle) * this.boxSize / 2;
-    let offsetZ = this.centerZ + sin(this.angle * 0.5) * this.boxSize / 2;
+    let offsetX = this.centerX + (sin(this.angle) * this.boxSize) / 2;
+    let offsetY = this.centerY + (cos(this.angle) * this.boxSize) / 2;
+    let offsetZ = this.centerZ + (sin(this.angle * 0.5) * this.boxSize) / 2;
 
     translate(offsetX, offsetY, offsetZ);
     rotateX(this.theta * 0.01);
@@ -33,7 +36,7 @@ class TorusMover {
 
     for (let i = 0; i < this.toriNumber; i++) {
       push();
-      let orbitAngle = (this.theta + i * TWO_PI / this.toriNumber) % TWO_PI;
+      let orbitAngle = (this.theta + (i * TWO_PI) / this.toriNumber) % TWO_PI;
       let orbitRadius = this.torusSize;
       let torusX = sin(orbitAngle) * orbitRadius;
       let torusY = cos(orbitAngle) * orbitRadius;
