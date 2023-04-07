1c40df6f4638f3b4ef021b510bbbb696322ac92c
catnip <davide.fiore@gmail.com>
2023-04-07 18:40:20 +0200

    tune parameters

--- sketches/ipnotic_torus/CyberPulseMorphosis.js
+++ sketches/ipnotic_torus/CyberPulseMorphosis.js
@@ -10,6 +10,13 @@ let frameCounterSpan;
 let timeCounterSpan;
 let thetaCounterSpan;
 
+let numberOfGroups = 3;
+let moversPerGroup = 3;
+let initialTorusSize = 40;
+let initialBoxSize = 100;
+let toriNumber = 3;
+let diameter = 350;
+
 function preload() {
   rotatingSkull = new RotatingSkull();
   glitchyBackground = new GlitchyBackground();
@@ -29,11 +36,7 @@ function setup() {
   colorMode(HSB, 360, 100, 100);
   frameRate(fps);
 
-  let boxSize = 200;
-  let torusSize = 40;
-  let toriNumber = 6;
-
-  let colors1 = [
+  let mainColors = [
     color(300, 100, 100), // Pink
     color(200, 100, 100), // Cyan
     color(60, 100, 100), // Yellow
@@ -41,7 +44,7 @@ function setup() {
   ];
 
   // Color scheme 2
-  let colors2 = [
+  let firstComplementaryColors = [
     color(327, 100, 100), // Hot Pink
     color(170, 100, 100), // Light Blue
     color(110, 100, 100), // Green
@@ -49,42 +52,27 @@ function setup() {
   ];
 
   // Color scheme 3
-  let colors3 = [
+  let secondComplementaryColors = [
     color(280, 100, 100), // Purple
     color(10, 100, 100), // Dark Red
     color(100, 100, 100), // Lime Green
     color(220, 100, 100), // Deep Sky Blue
   ];
 
-  for (let i = 0; i < 3; i++) {
-    let mover = new SpiralMover(
-      torusSize,
-      toriNumber,
-      boxSize,
-      colors1
-    );
-    movers.push(mover);
-  }
+  let colorsArray = [
+    mainColors,
+    firstComplementaryColors,
+    secondComplementaryColors,
+  ];
 
-  for (let i = 0; i < 3; i++) {
-    let mover = new SpiralMover(
-      torusSize * 1.5,
-      toriNumber,
-      boxSize * 1.5,
-      colors2
-    );
-    movers.push(mover);
-  }
-
-  for (let i = 0; i < 3; i++) {
-    let mover = new SpiralMover(
-      torusSize * 2,
-      toriNumber,
-      boxSize * 2,
-      colors3
-    );
-    movers.push(mover);
-  }
+  movers = createCircularMoverGroups(
+    numberOfGroups,
+    moversPerGroup,
+    toriNumber,
+    initialTorusSize,
+    initialBoxSize,
+    colorsArray
+  );
 
   createLoop({
     duration: loopDurationSeconds,
@@ -93,7 +81,7 @@ function setup() {
       download: true,
       options: {
         workers: 10,
-        dither: "Atkinson"
+        dither: "Atkinson",
       },
     },
   });
@@ -110,19 +98,59 @@ function draw() {
   glitchyBackground.display();
 
   for (const mover of movers) {
-    mover.update(theta);
+    let angleDifference = (TWO_PI / toriNumber) * movers.indexOf(mover);
+    mover.update(theta + mover.offsetAngle + angleDifference);
     mover.display();
   }
-  
 
+  setLightsForModel();
+  rotatingSkull.display();
+
+  updateUi(theta);
+}
+
+function createCircularMoverGroups(
+  numGroups,
+  moversPerGroup,
+  toriNumber,
+  torusSize,
+  boxSize,
+  colorsArray,
+  diameter
+) {
+  let movers = [];
+
+  for (let groupIdx = 0; groupIdx < numGroups; groupIdx++) {
+    let scaleFactor = 1 + groupIdx * 0.5;
+    let groupColors = colorsArray[groupIdx % colorsArray.length];
+
+    for (let i = 0; i < moversPerGroup; i++) {
+      let offsetAngle = TWO_PI / numGroups;
+      let mover = new CircularMover(
+        torusSize * scaleFactor,
+        toriNumber,
+        boxSize * scaleFactor,
+        groupColors,
+        offsetAngle,
+        diameter
+      );
+      movers.push(mover);
+    }
+  }
+
+  return movers;
+}
+
+function updateUi(theta) {
+  frameCounterSpan.innerText = `Frame: ${frameCount}`;
+  timeCounterSpan.innerText = `Time: ${(millis() / 1000).toFixed(1)}s`;
+  thetaCounterSpan.innerText = `Theta: ${theta.toFixed(2)}`;
+}
+
+function setLightsForModel() {
   ambientLight(50, 50, 50);
   pointLight(255, 0, 255, -200, 200, 200); // Pink light
   pointLight(0, 255, 255, 200, 200, 200); // Cyan light
   pointLight(255, 255, 0, -200, -200, 200); // Yellow light
   pointLight(255, 0, 0, 200, -200, 200); // Red light
-  rotatingSkull.display();
-  
-  frameCounterSpan.innerText = `Frame: ${frameCount}`;
-  timeCounterSpan.innerText = `Time: ${(millis() / 1000).toFixed(1)}s`;
-  thetaCounterSpan.innerText = `Theta: ${theta.toFixed(2)}`;
 }
